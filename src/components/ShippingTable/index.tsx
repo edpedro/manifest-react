ba1ac2baade3a253ae8353d2dd5f21b09c8d762b
Eduardo pedro <edp2013.ep@gmail.com>
2025-07-08 10:22:32 -0300

    Update index.tsx
    correção para admin, atualizar o processo de finalização

--- src/components/ShippingTable/index.tsx
+++ src/components/ShippingTable/index.tsx
@@ -132,7 +132,7 @@ export function ShippingTable() {
                     <TableCell>
                       <div className="flex items-center space-x-1">
                         {authData?.type !== "driver" ? (
-                          invoice.status === "Pendente" ? (
+                          invoice.isConfirm === false ? (
                             <Button
                               variant="ghost"
                               size="sm"
@@ -146,15 +146,14 @@ export function ShippingTable() {
                             <Button
                               variant="ghost"
                               size="sm"
-                              className="flex items-center gap-1 px-2 py-1"
-                              onClick={
+                              className="flex items-center gap-1 px-2 py-1"                             
+                            >
+                              <Truck className="h-4 w-4 text-green-600" />
+                              <span className="text-xs"  onClick={
                                 authData?.type === "admin"
                                   ? () => handleFinish(invoice.id)
                                   : undefined
-                              }
-                            >
-                              <Truck className="h-4 w-4 text-green-600" />
-                              <span className="text-xs">Finalizado</span>
+                              }>Finalizado</span>
                             </Button>
                           )
                         ) : (
